5343c759b6cbd4190ec2dc75f565e8628a1fc94b
mehulpruthi <latamakhija99@gmail.com>
2024-09-16 15:09:09 +0530

    new code added

---
+++ src/Sept/exe14092024/Lab003.java
@@ -0,0 +1,17 @@
+package Sept.exe14092024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+
+
+        int a = 20;
+        int b = 100;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+
+    }
+}
