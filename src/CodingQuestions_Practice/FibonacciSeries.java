01504d27172015ab6d6efc2b13a3053271ee7e44
mehulpruthi <latamakhija99@gmail.com>
2024-11-13 17:27:30 +0530

    new code added

---
+++ src/CodingQuestions_Practice/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package CodingQuestions_Practice;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number of terms");
+        int n=scanner.nextInt();
+        int firstTerm=0,secondTerm=1;
+        System.out.println("Fibonacci series is");
+        for(int i=1;i<=n;++i){
+            System.out.print(firstTerm+" ");
+            int nextTerm=firstTerm+secondTerm;
+            firstTerm=secondTerm;
+            secondTerm=nextTerm;
+        }
+        scanner.close();
+    }
+
+
+}
